1e3b333eccccd18f234ec96e7004afe8e15a3d14
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-08 09:35:09 -0800

    push

---
+++ wp-content/themes/kovacichsnipes/homepage_template_parts/section-1.php
@@ -0,0 +1,39 @@
+<section id="section_one">
+	
+	<div class="section_one_left">
+		
+		
+		
+	</div><!-- section_one_left -->
+	
+	<div class="section_one_right">
+		
+		<span class="large_header">We take on the most <strong>complex personal injury</strong> claims.</span><!-- large_header -->
+		
+		<a class="start_button" href="">start free case consultation</a><!-- start_button -->
+		
+		<div class="sp_slideshow">
+			
+			<div class="sp_single_slide">
+				
+				<div class="sp_svg">
+				
+					<?php echo file_get_contents("wp-content/themes/kovacichsnipes/images/new_sp01.svg"); ?>
+				
+				</div><!-- sp_svg -->
+				
+				<div class="sp_content_wrapper">
+					
+					<span class="sp_title"><span class="orangered number">01</span>Record Results</span><!-- sp_title -->
+					
+					<span class="sp_content">Our attorneys are well-known for our ability to get results in the courtroom. We’ve recovered over $300 million for our clients.</span><!-- sp_content -->
+					
+				</div><!-- sp_content -->
+				
+			</div><!-- sp_single_slide -->
+			
+		</div><!-- sp_slideshow -->
+		
+	</div><!-- section_one_right -->
+	
+</section><!-- section_one -->
