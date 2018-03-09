41da5435826ab747ce63110697e0b949182c5fed
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-09 14:49:28 -0800

    push

---
+++ wp-content/themes/kovacichsnipes/homepage_template_parts/section-2.php
@@ -0,0 +1,59 @@
+<section id="section_two">
+	
+	<span class="gold_line"></span><!-- gold_line -->
+	
+	<h1 class="intro">Montana Personal Injury Attorneys</h1>
+	
+	<div class="sec_two_content content">
+		
+
+		<div class="intro_block">
+			
+			
+			<?php echo file_get_contents("wp-content/themes/kovacichsnipes/images/new-mountain.svg"); ?>
+			
+			<span class="large_header">Experienced trial attorneys</span><!-- large_header -->
+			
+			<span class="blockquote">For more than three decades, our attorneys have been aggressive advocates for individuals who have been injured due to the negligence of others. We have a reputation as a firm that takes on the toughest cases, going up against some of the largest corporations in the nation.</span><!-- blockquote -->
+			
+		</div><!-- intro_block -->
+		
+			
+		<div class="col_one">
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+<h2>Heading Style 2</h2>
+
+<p>Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst.</p>
+		
+		
+<h3>Heading Style 3</h3>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Embedded Link Embedded Link  Rollover leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. Cumn ullamcorper massa dapibus.</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non</p>
+ 
+									
+			</div><!-- col_one -->
+			
+			
+			<div class="col_three">
+									
+					<h2>Lorem metus puraus amer numm donec tempor </h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+
+<h2>Fame malesuade metus puraus america numm donec tempor</h2>
+
+
+<p>Lorem elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst.</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst.</p> 
+											
+				</div><!-- col -->
+		
+		
+	</div><!-- sec_two_content -->
+	
+</section><!-- section_two -->
