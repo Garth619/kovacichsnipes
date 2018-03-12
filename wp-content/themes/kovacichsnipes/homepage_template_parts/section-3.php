13b159775f87860e975df3f9d9ccbb9b452c3d65
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-12 10:56:31 -0700

    push

---
+++ wp-content/themes/kovacichsnipes/homepage_template_parts/section-3.php
@@ -0,0 +1,32 @@
+<section id="section_three">
+	
+	
+	<div class="sec_three_left">
+		
+		<div class="peace_wrapper">
+			
+			<?php echo file_get_contents("wp-content/themes/kovacichsnipes/images/new-peace.svg"); ?>
+			
+		</div><!-- peace_wrapper -->
+		
+		<div class="sec_three_results_wrapper">
+			
+			<span class="large_header">We’ve recovered over $300 million  for clients</span><!-- large_header -->
+			
+			
+			<span class="results_content">Because of our history of success inside the courtroom, our peers in the legal community will frequently reach out to us when they have difficult cases.</span><!-- results_content -->
+			
+			
+		</div><!-- sec_three_results_wrapper -->
+		
+		
+	</div><!-- sec_three_left -->
+	
+	
+	<div class="sec_three_right">
+		
+		
+	</div><!-- sec_three_right -->
+	
+	
+</section><!-- section_three -->
