6176bf568408991b14e4edab51fe8e92ba56103a
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-12 14:37:39 -0700

    push

---
+++ wp-content/themes/kovacichsnipes/homepage_template_parts/section-4.php
@@ -0,0 +1,56 @@
+<section id="section_four">
+	
+	
+	<div class="single_result sr_one">
+		
+		<div class="results_inner">
+		
+			<?php echo file_get_contents("wp-content/themes/kovacichsnipes/images/new-result01.svg"); ?>
+		
+			<span class="amount">$41.2m</span><!-- amount -->
+		
+			<span class="case_type">Environmental Contamination</span><!-- case_type -->
+		
+			<span class="result_title">Verdict</span><!-- result_title -->
+		
+		</div><!-- results_inner -->
+		
+	</div><!-- single_result -->
+	
+	<div class="single_result sr_two">
+		
+		<div class="results_inner">
+		
+			<?php echo file_get_contents("wp-content/themes/kovacichsnipes/images/new-result02.svg"); ?>
+		
+			<span class="amount">$36m</span><!-- amount -->
+		
+			<span class="case_type">soil & water contamination</span><!-- case_type -->
+		
+			<span class="result_title">settlement</span><!-- result_title -->
+			
+			<a class="view_more_button" href="">View More</a><!-- view_more_button -->
+		
+		</div><!-- results_inner -->
+		
+	</div><!-- single_result -->
+	
+	<div class="single_result sr_three">
+		
+		<div class="results_inner">
+		
+			<?php echo file_get_contents("wp-content/themes/kovacichsnipes/images/new-result03.svg"); ?>
+		
+			<span class="amount">$13.75m</span><!-- amount -->
+		
+			<span class="case_type">traumatic brain injury</span><!-- case_type -->
+		
+			<span class="result_title">Verdict</span><!-- result_title -->
+		
+		</div><!-- results_inner -->
+		
+	</div><!-- single_result -->
+	
+		
+	
+</section><!-- section_four -->
