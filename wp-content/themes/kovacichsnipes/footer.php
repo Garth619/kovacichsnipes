991ce9162203180bd8c57a2dec61ff723ced8c4f
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-13 16:39:30 -0700

    push

--- wp-content/themes/kovacichsnipes/footer.php
+++ wp-content/themes/kovacichsnipes/footer.php
@@ -1,3 +1,23 @@
+<footer>
+	
+	<div class="footer_inner">
+		
+		<span class="footer_title">Get Started for Free</span><!-- footer_title -->
+		
+		<span class="footer_description">There is absolutely no risk to you to talk to us about your case in a free consultation. There are no attorneys' fees unless we are able to successfully recover compensation on your behalf. </span><!-- footer_description -->
+		
+		<div class="form_wrapper">
+			
+			<?php gravity_form(1, false, false, false, '', true, 245); ?>
+			
+		</div><!-- form_wrapper -->
+		
+	</div><!-- footer_inner -->
+	
+	
+</footer>
+
+
 
 
 <?php wp_footer();?>
