9b0a2c0a54d38867cc7e411fe12849b8d44654e3
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-13 13:03:48 -0700

    push

--- wp-content/themes/kovacichsnipes/homepage_template_parts/section-5.php
+++ wp-content/themes/kovacichsnipes/homepage_template_parts/section-5.php
@@ -13,7 +13,23 @@
 			
 			<div class="sec_five_content content">
 				
+				<div class="sec_five_col">
 				
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis.</p>
+
+					<h2>Fame malesuade metus puraus america numm donec tempor</h2>
+
+					<p>Det malesuada fames ac ante ipsum primis in faucibus. Proin blandit, lorem ipsum dolor sit amet, interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed</p>
+					
+				</div>
+					
+					<div class="sec_five_col">
+				
+					<p>Det malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis.</p>
+
+					<p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. ras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor.</p>
+				
+				</div>
 				
 			</div><!-- sec_five_content -->
 			
