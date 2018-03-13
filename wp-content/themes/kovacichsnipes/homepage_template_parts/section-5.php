3b10a6b7ac7680b1f73716786275dfff81bdf5d2
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-13 11:03:16 -0700

    push

---
+++ wp-content/themes/kovacichsnipes/homepage_template_parts/section-5.php
@@ -0,0 +1,26 @@
+<section id="section_five">
+	
+	
+	<div class="sec_five_inner">
+		
+		
+		<span class="sec_five_intro">When you work with us, you have a law firm that cares about you and your future, and is committed to helping you recover maximum compensation.</span><!-- sec_five_intro -->
+		
+		<div class="sec_five_content_wrapper">
+			
+			
+			<span class="sidenote">sa dapibus. Nullam  cam scelerisque</span><!-- sidenote -->
+			
+			<div class="sec_five_content content">
+				
+				
+				
+			</div><!-- sec_five_content -->
+			
+		</div><!-- sec_five_content -->
+		
+		
+	</div><!-- sec_five_inner -->
+	
+	
+</section><!-- section_five -->
