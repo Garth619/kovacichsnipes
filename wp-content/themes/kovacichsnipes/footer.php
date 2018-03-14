e376b3759354b80228a9b7931eae7bf2206741bd
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-14 09:23:32 -0700

    push

--- wp-content/themes/kovacichsnipes/footer.php
+++ wp-content/themes/kovacichsnipes/footer.php
@@ -1,19 +1,48 @@
 <footer>
 	
-	<div class="footer_inner">
-		
-		<span class="footer_title">Get Started for Free</span><!-- footer_title -->
-		
-		<span class="footer_description">There is absolutely no risk to you to talk to us about your case in a free consultation. There are no attorneys' fees unless we are able to successfully recover compensation on your behalf. </span><!-- footer_description -->
-		
-		<div class="form_wrapper">
-			
-			<?php gravity_form(1, false, false, false, '', true, 245); ?>
-			
-		</div><!-- form_wrapper -->
-		
-	</div><!-- footer_inner -->
+	<div class="form_outer">
 	
+		<div class="form_inner">
+		
+			<span class="footer_title">Get Started for Free</span><!-- footer_title -->
+		
+			<span class="footer_description">There is absolutely no risk to you to talk to us about your case in a free consultation. There are no attorneys' fees unless we are able to successfully recover compensation on your behalf. </span><!-- footer_description -->
+		
+			<div class="form_wrapper">
+			
+				<?php gravity_form(1, false, false, false, '', true, 245); ?>
+			
+				<span class="required"><span>*</span>Required</span><!-- required -->
+			
+			</div><!-- form_wrapper -->
+		
+		</div><!-- form_inner -->
+	
+	</div><!-- form_outer -->
+	
+	<div class="footer_location">
+		
+		
+		<span class="footer_goldline"></span><!-- footer_goldline -->
+		
+		<span class="firm_name">Kovacich Snipes</span><!-- firm_name -->
+		
+		
+		<a class="footer_adress" href="">725 3rd Ave. North Great Falls, MT 59401</a><!-- footer_adress -->
+		
+		<a class="footer_phone" href="tel;(406) 564-1743">(406) 564-1743</a><!-- footer_phone -->
+		
+		
+		<ul class="social_links">
+			
+			<li><a href="">Google+</a></li>
+			<li><a href="">Facebook</a></li>
+			<li><a href=""><Linkedin</a></li>
+			
+		</ul><!-- social_links -->
+		
+		
+	</div><!-- footer_location -->
 	
 </footer>
 
