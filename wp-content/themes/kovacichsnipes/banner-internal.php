04c22153f82c1c9a4f37f939bfd6f9b5de695285
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-15 15:15:40 -0700

    push

---
+++ wp-content/themes/kovacichsnipes/banner-internal.php
@@ -0,0 +1,21 @@
+<div class="int_banner_wrapper">
+	
+	
+	<div class="int_banner_image"></div><!-- int_banner_image -->
+	
+	
+	<div class="int_banner_content_wrapper">
+		
+		<div class="int_banner_content_inner">
+			
+				<span class="int_banner_content">We take on the most <strong>complex personal injury</strong> claims.</span><!-- int_banner_content -->
+		
+				<a class="int_start_button" href="">Start Free Case Consultation</a><!-- int_start_button -->
+			
+		</div><!-- int_banner_content_inner -->
+	
+	
+	</div><!-- int_banner_content_wrapper -->
+	
+
+</div><!-- int_banner_wrapper -->
--- wp-content/themes/kovacichsnipes/page.php
+++ wp-content/themes/kovacichsnipes/page.php
@@ -1,13 +1,26 @@
 <?php get_header(); ?>
 
 
+	<?php get_template_part( 'banner', 'internal' ); ?>
+	
+	
+	<div class="int_main">
+		
+		
+		
+		
+	</div><!-- int_main -->
 
-<?php get_template_part( 'loop', 'index' );?>
+
+
+<?php get_footer(); ?>
+
+
+
+
 
 <?php //use get_template_part( 'loop', 'page' ) if needed ?>
 
 	
 
 <?php // get_sidebar(); ?>
-
-<?php get_footer(); ?>
