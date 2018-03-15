028400353b73a47d5517eda96c0596f6f15ccabf
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-15 16:29:37 -0700

    push

--- wp-content/themes/kovacichsnipes/page.php
+++ wp-content/themes/kovacichsnipes/page.php
@@ -4,9 +4,52 @@
 	<?php get_template_part( 'banner', 'internal' ); ?>
 	
 	
-	<div class="int_main">
+	<div class="int_main two_column">
 		
 		
+		<?php get_sidebar(); ?>
+		
+		<div class="container content">
+			
+			
+			<h1 class="int_intro">Utah Car Accident Attorneys</h1><!-- int_intro -->
+			
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			
+			<h2>Heading Style 2</h2>
+
+			<p>Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst.</p>
+
+			<h3>Heading Style 3 </h3>	
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Embedded Link Embedded Link Rollover leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. In hac habitasse platea dictumst. Cumn ullamcorper massa dapibus.</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibus. Nullam scelerisque leo vel tempor tempor. Proin pretium euismod ipsum. Proin ullamcorper molestie velit ut sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin blandit, nisl quis pulvinar lacinia, justo metus ornare lorem, et pellentesque purus metus et nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan sapien sed metus faucibus, non ullamcorper massa dapibu.</p>
+
+
+<blockquote>Pull Quote - For more than three decades, our attorneys have been aggressive advocates for individuals who have been injured due to the negligence of others. We have a reputation as a firm that takes on the toughest cases, going up against some of the largest corporations in the nation.</blockquote>
+
+	<h4>Heading Style 4</h4>
+
+	<p>Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut. Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut. Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia.</p>
+	
+	<ul>
+		<li>Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat.
+		<li>Neue nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque  cursus velit a gravida tempor. 
+		<li>Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. Nullam in dui tincidunt, dignissim lorem ut.
+	</ul>
+
+<p>Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut. Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia. Pellentesque ullamcorper tincidunt accumsan. Cras finibus tortor non lacus lacinia varius. Sed nec scelerisque nibh. Aenean feugiat congue quam, sed scelerisque ex luctus ut. Nunc bibendum molestie mauris, sed semper nisi sodales quis. Morbi imperdiet orci ut viverra lacinia.</p>
+
+<ol>
+	<li>Fusce fringilla vitae risus vestibulum volutpat. Curabitur semper scelerisque dui in interdum. Suspendisse eleifend finibus viverra. Quisque imperdiet, odio vitae condimentum volutpat.</li>
+	<li>Neue nisl suscipit risus, nec fringilla mauris tortor sit amet justo. Donec bibendum iaculis magna, eu auctor nisi placerat ut. Quisque  cursus velit a gravida tempor. </li>
+	<li>Vestibulum vel eleifend dolor. In laoreet ultricies tellus id mollis. Nullam in dui tincidunt, dignissim lorem ut.</li>
+</ol>
+			
+			
+		</div><!-- container -->
 		
 		
 	</div><!-- int_main -->
@@ -23,4 +66,3 @@
 
 	
 
-<?php // get_sidebar(); ?>
--- wp-content/themes/kovacichsnipes/sidebar.php
+++ wp-content/themes/kovacichsnipes/sidebar.php
@@ -1,5 +1,21 @@
+<div class="sidebar">
+	
+	
+	
+	<ul>
+		<li><a class="" href="">Practice Areas</a></li>
+		<li><a class="" href="">Practice Areas</a></li>
+		<li><a class="" href="">Practice Areas</a></li>
+		<li><a class="" href="">Practice Areas</a></li>
+		<li><a class="" href="">Practice Areas</a></li>
+		<li><a class="" href="">Practice Areas</a></li>
+		<li><a class="" href="">Practice Areas</a></li>
+	</ul>
+	
+	
+	
+</div><!-- sidebar -->
 
 
-
-	<?php dynamic_sidebar( 'sidebar' ); ?>
+	<?php // dynamic_sidebar( 'sidebar' ); ?>
 			
