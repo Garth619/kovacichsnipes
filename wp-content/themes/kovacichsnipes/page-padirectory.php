cf985d09922e6bfcc4f590bb12a5521466a633f0
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-16 15:49:18 -0700

    push

--- wp-content/themes/kovacichsnipes/page-padirectory.php
+++ wp-content/themes/kovacichsnipes/page-padirectory.php
@@ -4,12 +4,15 @@
 	
 	get_header(); ?>
 	
-	<div class="int_main">
+	<div class="int_main one_col">
 		
 		<h1 class="internal_header_mount">Practice Areas</h1><!-- internal_header_mount -->
 		
 		
 		<div class="pa_directory_wrapper">
+			
+			
+			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
 			
 			
 		</div><!-- pa_directory_wrapper -->
