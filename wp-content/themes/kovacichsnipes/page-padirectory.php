7ab0eba9fcfa931ea74dad645186a1eef87fb511
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-16 15:01:46 -0700

    push

---
+++ wp-content/themes/kovacichsnipes/page-padirectory.php
@@ -0,0 +1,32 @@
+<?php 
+	
+	/* Template Name: Pa Directory (Sub-Pages) */
+	
+	get_header(); ?>
+	
+	<div class="int_main">
+		
+		<h1 class="internal_header_mount">Practice Areas</h1><!-- internal_header_mount -->
+		
+		
+		<div class="pa_directory_wrapper">
+			
+			
+		</div><!-- pa_directory_wrapper -->
+		
+		
+		
+	</div><!-- int_main -->
+
+
+
+<?php get_footer(); ?>
+
+
+
+
+
+<?php //use get_template_part( 'loop', 'page' ) if needed ?>
+
+	
+
