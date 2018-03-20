19bc62f7062b3c83a416724bc31d2a9b15e7905c
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-20 16:27:12 -0700

    push

--- wp-content/themes/kovacichsnipes/404.php
+++ wp-content/themes/kovacichsnipes/404.php
@@ -10,17 +10,12 @@
 			
 			<span class="error_title">Page Not Found</span><!-- error_title -->
 			
-			<span class="error_content">The page you were looking for appears to have been moved, deleted or does not exist.<br/> You could go back to where you were or head straight to our home page</span><!-- error_content -->
+			<span class="error_content">The page you were looking for appears to have been moved, deleted or does not exist.<br/> You could go back to where you were or head straight to our <a href="<?php bloginfo('url');?>">home page</a></span><!-- error_content -->
 			
 		</div><!-- error_wrapper -->
 
 		
-		<div class="container content">
-			
-			
-						
-		</div><!-- container -->
-		
+				
 		
 	</div><!-- int_main -->
 
---
+++ wp-content/themes/kovacichsnipes/page-meetteam.php
@@ -0,0 +1,200 @@
+<?php 
+	
+	/* Template Name: Meet the Team */
+	
+	get_header(); ?>
+	
+	<div class="int_main one_col">
+		
+		<h1 class="internal_header_mount"><?php the_title();?></h1><!-- internal_header_mount -->
+		
+		<div class="cr_intro_page">
+			
+			<div class="cr_intro_content">
+				
+				<p>We have a reputation as a firm that takes on the toughest cases, going up against some of the largest corporations in the nation. Many of our attorneys have received very prestigious awards and honors from our peers in the local legal community.</p>
+				
+			</div><!-- cr_intro_content -->
+			
+		</div><!-- cr_intro_page -->
+		
+		<div class="meet_the_team">
+			
+			
+			<div class="single_att_wrapper">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/intl-profile-kov.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			<div class="single_att_wrapper no-profile">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/placeholder.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			<div class="single_att_wrapper">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/intl-profile-kov.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			<div class="single_att_wrapper">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/intl-profile-kov.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			<div class="single_att_wrapper">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/intl-profile-kov.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			<div class="single_att_wrapper">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/intl-profile-kov.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			<div class="single_att_wrapper">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/intl-profile-kov.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			<div class="single_att_wrapper">
+				
+				<a href="">
+				
+					<div class="single_att_inner">
+						
+						<div class="single_att_overlay"></div><!-- single_att_overlay -->
+						
+						<img src="<?php bloginfo('template_directory');?>/images/intl-profile-kov.jpg"/>
+					
+					</div><!-- single_att_inner -->
+					
+					<span class="single_att_title">Mark M.Kovacich</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att_wrapper -->
+			
+			
+			
+			
+		</div><!-- meet_the_team -->
+		
+		
+		
+		
+	</div><!-- int_main -->
+
+
+<?php get_footer(); ?>
+
+
+
+	
+
