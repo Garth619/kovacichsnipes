4f779c1caf01f3a9b34467d8c8fc0b94a3633d89
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-21 13:50:10 -0700

    push

---
+++ wp-content/themes/kovacichsnipes/page-profile.php
@@ -0,0 +1,120 @@
+<?php
+	
+	
+/* Template Name: Profile */
+
+get_header(); ?>
+	
+	
+	
+	<?php // get_template_part( 'banner', 'internal' ); ?>
+	
+	
+	<div class="int_main">
+		
+		<div class="profile_wrapper">
+		
+		<div class="profile_img_wrapper">
+				
+				<img class="profile_page_img" src="<?php bloginfo('template_directory');?>/images/intl-profile-snipes.jpg"/>
+				
+			</div><!-- profile_img_wrapper -->
+		
+		
+		<div class="container content">
+			
+			
+			
+			<h1 class="int_intro"><?php the_title();?></h1><!-- int_intro -->
+			
+			<span class="profile_position">Partner</span><!-- profile_position -->
+			
+			
+			<p>Ben Snipes is a plaintiff's personal injury attorney, specializing in toxic tort claims and employment injuries. His practice presently focuses on asbestos related diseases, mesothelioma cases and catastrophic work injuries. In addition to a traditional tort practice, Mr. Snipes carries a significant Qui Tam/False Claims Act whistleblower and workers' compensation caseload. </p>
+			
+			<p>He is an AV Preeminent rated attorney by Martindale-Hubbell® and has been recognized as one of the Top 100 Trial Lawyers in Montana by The National Trial Lawyers® since 2014. He has participated in educational seminars and published articles for the Montana Trial Lawyers Association. Mr. Snipes has been a shareholder with Kovacich Snipes, P.C., since 2015 and is admitted to practice in all Montana federal and state courts and the 9th Circuit Court of Appeals.</p>
+
+			<blockquote>Cras ac iaculis elit. In facilisis ipsum nec tempor malesuada. Proin in fringilla nisi. Duis fermentum suscipit metus vitae euismod.</blockquote>
+			
+			<p>Ben grew up in Great Falls. He graduated from C.M. Russell High School, and accepted a football scholarship at the University of Montana-Western in Dillon, Montana. Ben enjoyed a successful athletic and academic career at Western, graduating with honors with a B.S. in Business Administration. Ben went on to attend the University of Montana-School of Law, where he graduated in 2008.While attending law school Ben worked at the Cascade County Attorney's Office and with the Missoula law firm of Paoli, Latino and Kutzman. He also interned at ASUM Legal Services, where he represented University of Montana students in a variety of legal matters. Ben was a student member of the American Association of Justice.</p>
+
+			<p>Ben enjoys fishing, hunting, golfing and spending time with his wife, Tori, and their children Elizabeth, Jack, and Calvin.</p>
+			
+			
+			<div class="accolades">
+				
+				<div class="accolades_col">
+				
+					<div class="single_accolade">
+					
+						<span class="accolade_title">Education</span><!-- accolade_title -->
+					
+						<ul>
+							<li>University of Montana School of Law, Missoula, Montana: J.D. - 2008</li>
+							<li>University of Montana - Western, Dillon, Montana: B.S. magna cum laude, Honors: With Honors, Honors: Football Scholarship, Major: Business Administration</li>
+						</ul>
+					
+					</div><!-- single_accolade -->
+				
+					<div class="single_accolade">
+					
+						<span class="accolade_title">Education</span><!-- accolade_title -->
+					
+						<ul>
+							<li>University of Montana School of Law, Missoula, Montana: J.D. - 2008</li>
+							<li>University of Montana - Western, Dillon, Montana: B.S. magna cum laude, Honors: With Honors, Honors: Football Scholarship, Major: Business Administration</li>
+						</ul>
+					
+					</div><!-- single_accolade -->
+				
+				</div><!-- accolades_col -->
+				
+				
+				<div class="accolades_col">
+				
+					<div class="single_accolade">
+					
+						<span class="accolade_title">Education</span><!-- accolade_title -->
+					
+						<ul>
+							<li>University of Montana School of Law, Missoula, Montana: J.D. - 2008</li>
+							<li>University of Montana - Western, Dillon, Montana: B.S. magna cum laude, Honors: With Honors, Honors: Football Scholarship, Major: Business Administration</li>
+						</ul>
+					
+					</div><!-- single_accolade -->
+				
+					<div class="single_accolade">
+					
+						<span class="accolade_title">Education</span><!-- accolade_title -->
+					
+						<ul>
+							<li>University of Montana School of Law, Missoula, Montana: J.D. - 2008</li>
+							<li>University of Montana - Western, Dillon, Montana: B.S. magna cum laude, Honors: With Honors, Honors: Football Scholarship, Major: Business Administration</li>
+						</ul>
+					
+					</div><!-- single_accolade -->
+				
+				</div><!-- accolades_col -->
+				
+				
+			</div><!-- accolades -->	
+			
+		</div><!-- container -->
+		
+		</div><!-- profile_wrapper -->
+		
+		
+	</div><!-- int_main -->
+
+
+
+<?php get_footer(); ?>
+
+
+
+
+
+<?php //use get_template_part( 'loop', 'page' ) if needed ?>
+
+	
+
