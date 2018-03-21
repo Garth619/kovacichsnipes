1d400615872a1cccda679bc98d7c0beb364a8b00
Garrett Cullen <garrettwcullen@gmail.com>
2018-03-21 16:27:54 -0700

    push

---
+++ wp-content/themes/kovacichsnipes/page-contact.php
@@ -0,0 +1,46 @@
+<?php 
+	
+
+/* Template Name: Contact */	
+	
+	
+get_header(); ?>
+	
+	
+	
+	<?php // get_template_part( 'banner', 'internal' ); ?>
+	
+	
+	<div class="int_main contact_outer">
+	
+		<div class="contact_left">
+			
+			
+		</div><!-- contact_left -->
+		
+		<div class="contact_right content">
+			
+			
+			<div class="contact_right_inner">
+			
+				<h1 class="contact_intro"><?php the_title();?></h1>
+			
+				<span class="contact_description">While this website provides general information, it does not constitute legal advice. The best way to get guidance on your specific legal issue is to contact a lawyer. To schedule a meeting with an attorney, please call or complete the intake form below.</span><!-- contact_description -->
+			
+			</div><!-- contact_right_inner -->
+			
+			
+		</div><!-- contact_right -->
+		
+	</div><!-- int_main -->
+
+
+
+<?php get_footer(); ?>
+
+
+
+<?php //use get_template_part( 'loop', 'page' ) if needed ?>
+
+	
+
